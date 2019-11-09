49800567c4d61b3f2006b21abde3de91b5129021
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-09 20:42:06 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -53,11 +53,14 @@ def naming_system
              label: "Family",
              sub_category: {
                label: "Genus",
-               
+               sub_category: {
+                 label: "Species",
+                 sub_category: nil
+               }
              }
            }
          }
        }
-     }
+      }
     }
 end
